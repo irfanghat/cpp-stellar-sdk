accd6707bb6febfae423ce17af7f13353ff58b76
irfanghat <irfanghat@gmail.com>
2026-09-15 10:45:22 +0000

    Include generated xdr headers

---
+++ include/stellar/types.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Stellar-contract-meta.hh>
+#include <Stellar-contract-spec.hh>
+#include <Stellar-contract.hh>
+#include <Stellar-ledger-entries.hh>
+#include <Stellar-ledger.hh>
+#include <Stellar-transaction.hh>
+#include <Stellar-types.hh>
